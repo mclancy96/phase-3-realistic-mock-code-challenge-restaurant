977146ff3af528cff01a595c221fc25beb611af5
Kyle <kylegranthousel@gmail.com>
2025-07-31 20:19:09 -0400

    Hi Mike

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,3 +1,15 @@
 class Customer < ActiveRecord::Base
+  has_many :orders
+  has_many :restaurants, through: :orders
 
+  def ordered_dish?(dish_name)
+    orders.where(dish_name: dish_name).exists?
+  end
+
+  def transfer_order(new_customer, order)
+    return unless self != new_customer
+
+    new_customer.orders << order
+    new_customer.save
+  end
 end
